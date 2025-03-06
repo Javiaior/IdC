2acd333d76f828e29561758e379781892370bc96
Javiaior <javiles.avior@gmail.com>
2025-03-06 01:30:24 +0000

    deleted:    app/favicon.ico 	deleted:    app/globals.css 	deleted:    app/layout.tsx 	deleted:    app/page.tsx 	modified:   pages/index.js

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,16 @@
 export default function Home() {
     return (
       <div>
-        <h1>Isla del Combate</h1>
-        <p>Welcome to Isla del Combate! Our mission is to support athletes, events, and education in the martial arts community.</p>
+        <h1>Que es Isla del Combate</h1>
+        <p>Isla del combate es un proyecto cuya mision es promover y fomentar no solo las artes marciales en la isla, sino tambien sus atletas, competidores y practicantes.</p>
+        <h1>Como intentamos hacerlo</h1>
+        <h2>Apoyo a atletas</h2>
+        <p>Existen muchos atletas y practicantes en la isla que compiten dentro y fuera de la isla. Buscamos apoyarlos en cualquier manera que podamos.</p>
+        <h2>Apoyo a eventos</h2>
+        <p>Cuando aparece una oportunidad de estar en un evento en la isla, decimos presente. Sea ayudando, grabando, tirando fotos, auspiciando o de espectadores, buscamos apoyar a los eventos y sus organizadores.</p>
+        <h2>Educacion</h2>
+        <p>A traves de analisis de tecnica y estrategias, asisitiendo  seminarios y visitas y apoyando eventos de compartir y aprendizaje entre atletas en la isla, buscamos alimentar el conocimiento de los practicantes de la isla, aumentando el conocimiento colectivo de la comunidad.</p>
+
       </div>
     );
   }
